728722342dab50211c0698116cfe55266c8ceaf5
you-tech-sys <62576535+you-tech-sys@users.noreply.github.com>
2022-12-14 21:46:09 +0100

    adding the monty.c file

--- monty.c
+++ monty.c
@@ -1,38 +1,20 @@
 #include "monty.h"
+
+glob_t global = {NULL, NULL};
 /**
- * main - entry point
- * @ac: ac
- * @av: av
- * Return: 0 if success.
+ * main - Entry point
+ * @argc: Number of arguments
+ * @argv: Arguments
+ * Return: number of arguments.
  */
-int main(int ac, char **av)
+int main(int argc, char *argv[])
 {
-	stack_t *head = NULL;
-	size_t size = 0;
-	char **str = NULL;
-	char *lineptr = NULL;
-	unsigned int linenum = 1;
-	FILE *fp;
-
-	if (ac != 2)
-		_perror(av[0], 0, 0);
-	fp = fopen(av[1], "r");
-	if (!fp)
-		_perror(av[1], 0, 1);
-	while (getline(&lineptr, &size, fp) != -1)
+	if (argc == 2)
+		handle_command(argv[1]);
+	else
 	{
-		str = tokenize(lineptr);
-		if (!str)
-		{
-			linenum++;
-			continue;
-		}
-		matcher(str, lineptr, &head, fp, linenum);
-		linenum++;
-
+		dprintf(STDERR_FILENO, "USAGE: monty file\n");
+		exit(EXIT_FAILURE);
 	}
-	free(lineptr);
-	free(head);
-	fclose(fp);
 	return (0);
 }
