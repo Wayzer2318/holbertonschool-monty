265d5a73e76b14843d15af1abfed33377f05efd6
Wayzer2318 <arezki.ait.kettout@gmail.com>
2023-05-14 23:35:14 +0200

    adding push function

--- opcode.c
+++ opcode.c
@@ -22,3 +22,23 @@ stack_t *new_Node(int n)
 
 	return (new);
 }
+
+(void)line_number;
+/**
+ * _push - push new node to stack
+ * @stack: stack
+ * @line_number: is a value
+ */
+void _push(stack_t **stack, unsigned int line_number)
+{
+  stack_t *new = NULL;
+  (void)line_number;
+  new = new_Node(value);
+
+  new->next = *stack;
+  if (*stack != NULL)
+  {
+    (*stack)->prev = new;
+  }
+   *stack = new;
+}
