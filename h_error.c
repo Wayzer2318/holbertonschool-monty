d089824aa01e0af99b6c8321415fe9e48a1be5cd
Wayzer2318 <arezki.ait.kettout@gmail.com>
2023-05-14 23:58:11 +0200

    updating the handle error file

--- h_error.c
+++ h_error.c
@@ -24,4 +24,12 @@ void push_error(FILE *fd, char *line, stack_t *stack, int count)
  * @count: is a line command
  * @item: number
  */
+void ins_error(FILE *fd, char *line, stack_t *stack, char *count, int item)
+{
+	dprintf(STDERR_FILENO, "L%u: unknown instruction %s\n", item, count);
+	fclose(fd);
+	free(line);
+	free_dlistint(stack);
+	exit(EXIT_FAILURE);
+}
 
