e20077f474ceab5ef3fa9b95d3049cf994d7352d
Wayzer2318 <arezki.ait.kettout@gmail.com>
2023-05-14 23:54:15 +0200

    adding the pall function

--- opcode.c
+++ opcode.c
@@ -23,22 +23,42 @@ stack_t *new_Node(int n)
 	return (new);
 }
 
-(void)line_number;
 /**
- * _push - push new node to stack
- * @stack: stack
- * @line_number: is a value
+ * _push - push item
+ * @stack: is a parameter
+ * @line_number: is value
  */
 void _push(stack_t **stack, unsigned int line_number)
 {
-  stack_t *new = NULL;
-  (void)line_number;
-  new = new_Node(value);
+	stack_t *new = NULL;
+	(void)line_number;
 
-  new->next = *stack;
-  if (*stack != NULL)
-  {
-    (*stack)->prev = new;
-  }
-   *stack = new;
+	new = new_Node(value);
+
+	new->next = *stack;
+	if (*stack != NULL)
+		(*stack)->prev = new;
+	*stack = new;
 }
+
+/**
+ * _pall - print elements stack
+ * @stack: is a parameter
+ * @n: is value
+ * Return: nothing
+ */
+void _pall(stack_t **stack, unsigned int n)
+{
+	stack_t *current = NULL;
+	(void)n;
+
+	current = *stack;
+
+	while (current != NULL)
+	{
+		dprintf(STDOUT_FILENO, "%d\n", current->n);
+		current = current->next;
+	}
+}
+
+
